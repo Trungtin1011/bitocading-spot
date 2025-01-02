c54892ec179257dc2df5d90683fb7911824ff57b
Tin Trung Ngo <trungtinth1011@gmail.com>
2025-01-02 17:32:45 +0700

    Update resources interaction functions
    Signed-off-by: Tin Trung Ngo <trungtinth1011@gmail.com>

--- practice/aws/resources_interaction.py
+++ practice/aws/resources_interaction.py
@@ -3,35 +3,165 @@
 # Resource API: An abstraction on top of the service HTTP API
 # Ref: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.html
 
-import botocore, boto3, os, logging
+import botocore, boto3, os, logging, json
+import re, ipaddress
 
-# Init logger and variables
+# Init logger
+logging.basicConfig(format="[%(levelname)s]\t%(message)s",level=logging.INFO)
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 
-# List of available services that can be loaded as low-level clients via Session.client()
-def fetch_available_services(profile:str):
+# List all available services that can be loaded with a profile
+def list_available_services(profile:str):
   try:
-    session = boto3.Session(profile_name=profile)
+    return [svc for svc in boto3.Session(profile_name=profile).get_available_services()]
   except Exception as e:
     logger.error(e.args[0])
-    return 1
+    return e.args[0]
 
-  logger.info([svc for svc in session.get_available_services()])
-  return 0
+# Check if provided IP is a valid private IP
+def is_valid_private_ip(ip:str):
+  try:
+      return ipaddress.ip_address(ip).is_private
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+# Check if provided AWS VPC ID is in valid format
+def is_valid_vpc_id(vpc:str):
+  valid_pattern = re.compile(r'vpc-\w{17}$')
+  try:
+    return True if valid_pattern.match(vpc) is not None else False
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+def detect_vpc_from_ip(ip:str):
+  try:
+    if is_valid_private_ip(ip):
+      valid_ip = ipaddress.ip_address(ip)
+      subnets_list:list = boto3.client('ec2').describe_subnets()['Subnets']
+
+      for subnet in subnets_list:
+        if valid_ip in ipaddress.ip_network(f"{subnet['CidrBlock']}"):
+          return f"Provided IP address belongs to subnet {subnet['SubnetId']}, VPC {subnet['VpcId']}"
+          break
+        else:
+          return "Provided IP address not belongs to any subnets"
+
+    else:
+      logger.error(f"Invalid IP address {ip}")
+      return f"Invalid IP address {ip}"
+
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+# List all S3 buckets in form of ['bucket']
+def list_buckets():
+  try:
+    buckets:list = boto3.client("s3").list_buckets()['Buckets']
+    #logger.info([bucket["Name"] for bucket in buckets])
+    return ([bucket["Name"] for bucket in buckets])
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+# List all objects from a bucket in form of ['object']
+def list_bucket_objects(bucket:str):
+  try:
+    return [object['Key'] for object in boto3.client("s3").list_objects(Bucket=bucket)['Contents']]
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+# List all tags from a bucket in form of ['key: value']
+def list_bucket_tags(bucket:str):
+  try:
+    return [tags['Key'] + ": " + tags['Value'] for tags in boto3.client("s3").get_bucket_tagging(Bucket=bucket)['TagSet']]
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
 
 # List S3 bucket through VPC endpoint URL (eg. "https://bucket.vpce-xxx-yyyy.s3.ap-southeast-1.vpce.amazonaws.com")
-def list_bucket_through_vpce(profile:str,region:str,endpoint:str):
+def list_bucket_through_vpce(region:str,endpoint:str):
   try:
-    s3_client = boto3.Session(profile_name=profile).client(service_name="s3", region_name=region, endpoint_url=endpoint)
+    s3_client = boto3.client(service_name="s3", region_name=region, endpoint_url=endpoint)
+    return ([bucket['Name'] for bucket in s3_client.list_buckets()['Buckets']])
   except Exception as e:
     logger.error(e.args[0])
-    return 1
+    return e.args[0]
 
+# Return a dict in format {"instance-id": {Tags} }
+def get_ec2_instance_tags():
+  instance_tags:dict = {}
   try:
-    logger.info([bucket["Name"] for bucket in s3_client.list_buckets()['Buckets']])
-    return 0
+    instances:list = []
+    ec2_instances = boto3.client('ec2').describe_instances()
+    [instances.append(ec2['Instances']) for ec2 in ec2_instances['Reservations']]
+
+    for instance in instances:
+      if "Tags" not in instance[0]:
+        #logger.info(f"Instance {instance[0]['InstanceId']} does not contains tags")
+        instance_tags[f"{instance[0]['InstanceId']}"] = "No tags"
+      else:
+        tagsset:dict = {}
+        for tag in instance[0]['Tags']:
+          tagsset[f"{tag['Key']}"] = f"{tag['Value']}"
+        #logger.info(f"Instance {instance[0]['InstanceId']} tags-> {tagsset}")
+        instance_tags[f"{instance[0]['InstanceId']}"] = f"{tagsset}"
+
+    return instance_tags
   except Exception as e:
     logger.error(e.args[0])
-    return 1
+    return e.args[0]
+
+# Check if each EC2 instance contains a specific tag, return list of result
+def check_ec2_tag_comply(tag:str):
+  try:
+    report:list = []
+    instance_tags = get_ec2_instance_tags()
+
+    for instance in instance_tags:
+      if tag not in instance_tags[f'{instance}']:
+        #logger.info(f"Instance {instance} does not contain tag {tag}")
+        report.append(f"Instance {instance} does not contain tag {tag}")
+      else:
+        #logger.info(f"Instance {instance} contains tag {tag}")
+        report.append(f"Instance {instance} contains tag {tag}")
+
+    return report
+
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+
+# Check if an EC2 instance contains a specific tag, return 1 or 0
+def check_ec2_tag_exist(instance_id:str,tag:str):
+  try:
+    ec2_instance = boto3.client('ec2').describe_instances(InstanceIds=[instance_id])['Reservations']
+    instance_tags = ec2_instance[0]['Instances'][0]['Tags']
+
+    tagsset:dict = {}
+    [tagsset.update({f"{tag['Key']}":f"{tag['Value']}"}) for tag in instance_tags]
+
+    if tag in tagsset:
+      #logger.info(f"Tag {tag} exist in instance {instance_id}")
+      return 0
+    else:
+      #logger.info(f"Tag {tag} does not exist in instance {instance_id}")
+      return 1
+
+  except Exception as e:
+    logger.error(e.args[0])
+    return e.args[0]
+
+#print(is_valid_vpc_id("vpc-0e84b8789fa69d0f8"))
+
+#print(is_valid_vpc_id("vpc-sdfsdf"))
+
+
+print(detect_vpc_from_ip("10.1.1.10"))
+
+# print(detect_vpc_from_ip("100.64.0.10"))
--- practice/aws/resources_scheduling.py
+++ practice/aws/resources_scheduling.py
@@ -1,10 +1,8 @@
 import botocore, boto3, os, logging
 
 # Init logger and variables
+logging.basicConfig(format="[%(levelname)s]\t%(message)s",level=logging.INFO)
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-eventbus = os.environ.get('BUS_NAME')
-prefix = os.environ.get('NAME_PREFIX')
 
 def eventbridge_rules_scheduler(eventbus:str,prefix:str):
     # Init EventBridge boto3 client
@@ -19,15 +17,20 @@ def eventbridge_rules_scheduler(eventbus:str,prefix:str):
       for rule in evb.list_rules(NamePrefix=prefix, EventBusName=eventbus)['Rules']:
           if (rule['State']) == "ENABLED":
               logger.info('Disabling rule: %s', rule['Name'])
-              evb.disable_rule(Name=rule['Name'], EventBusName=eventbus)
-              return 0
+              #evb.disable_rule(Name=rule['Name'], EventBusName=eventbus)
           elif (rule['State']) == "DISABLED":
               logger.info('Enabling rule: %s', rule['Name'])
-              evb.enable_rule(Name=rule['Name'], EventBusName=eventbus)
-              return 0
+              #evb.enable_rule(Name=rule['Name'], EventBusName=eventbus)
           else:
               logger.info('Cannot determine EventBridge rule status')
-              return 1
     except Exception as e:
         logger.error(e.args[0])
         return 1
+
+
+
+def main():
+  eventbus = os.environ.get('BUS_NAME')
+  prefix = os.environ.get('NAME_PREFIX')
+
+  eventbridge_rules_scheduler(f"{eventbus}",f"{prefix}")
